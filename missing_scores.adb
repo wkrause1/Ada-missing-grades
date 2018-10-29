397456cab4ef74fa7aa6eab96831d209acdd80ba
Will Krause <wkrause1@radford.edu>
2018-10-29 15:26:36 -0400

    Added input file, started get_student procedure

--- missing_scores.adb
+++ missing_scores.adb
@@ -12,12 +12,12 @@ type program_grades is record
 end record;
 type quiz_grades is record
     num_grades: Natural;
-    grades_array: grades;
+    grades_array: grades:= (others => 0);
     percent_total: Natural;
 end record;
 type test_grades is record
     num_grades: Natural;
-    grades_array: grades;
+    grades_array: grades:= (others => 0);
     percent_total: Natural;
 end record;
 type exam_grades is record
@@ -35,22 +35,41 @@ type student is record
     overall_average: float;
     letter_grade: Character;
 end record;
+type student_array is array(1..100) of student;
+
 
 procedure make_records(programs: out program_grades; quizzes: out quiz_grades; tests: out test_grades; exams: out exam_grades) is
 begin
 get(programs.num_grades);
+put_line(programs.num_grades'img);
 get(programs.percent_total);
+put_line(programs.percent_total'img);
 get(quizzes.num_grades);
+put_line(quizzes.num_grades'img);
 get(quizzes.percent_total);
 get(tests.num_grades);
 get(tests.percent_total);
 get(exams.num_grades);
 get(exams.percent_total);
+skip_line;
 end make_records;
 
-
+procedure make_students(stu_array: in out student_array) is
+temp: student;
 begin
-null;
+get_line(temp.name, temp.letters_in_name);
+put_line(temp.name);
+
+end make_students;
+
+programs: program_grades;
+quizzes: quiz_grades;
+tests: test_grades;
+exams: exam_grades;
+stu_array: student_array;
+begin
+make_records(programs, quizzes, tests, exams);
+make_students(stu_array);
 end missing_scores;
 
 
