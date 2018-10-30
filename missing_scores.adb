cba4f481c29d33adc73b004d40bfb10f2672d853
Will Krause <wkrause1@radford.edu>
2018-10-30 19:06:53 -0400

    Working, time to refactor

--- missing_scores.adb
+++ missing_scores.adb
@@ -109,6 +109,13 @@ function compute_exam_points(s: student) return Integer is
     return points;
 end compute_exam_points;
 
+function compute_overall_average(s:student) return Integer is
+    sum: Integer;
+    begin
+    sum := s.ppoints + s.qpoints + s.tpoints + s.epoints;
+    return sum;
+end compute_overall_average;
+
 procedure fill_in_missing_test_grade(s: in out student) is
     begin
     for i in 1..s.tgrades.num_grades loop
@@ -120,17 +127,15 @@ end fill_in_missing_test_grade;
 
 
 function computer_letter_grade (s: student) return Character is
-    sum : Float;
     grade: Character;
     begin
-    sum := Float(s.ppoints + s.qpoints + s.tpoints + s.epoints);
-    if sum >= 90.0 then
+    if s.overall_average >= 90 then
         grade := 'A';
-    elsif sum >= 80.0 then
+    elsif s.overall_average >= 80 then
         grade := 'B';
-    elsif sum >= 70.0 then
+    elsif s.overall_average >= 70 then
         grade := 'C';
-    elsif sum >= 60.0 then
+    elsif s.overall_average >= 60 then
         grade := 'D';
     else
         grade := 'F';
@@ -154,12 +159,13 @@ get(exams.percent_total);
 skip_line;
 end make_records;
 
-procedure make_students(stu_array: in out student_array; programs: program_grades; quizzes: quiz_grades; tests: test_grades; exams: exam_grades) is
+procedure make_students(stu_array: in out student_array; programs: program_grades; quizzes: quiz_grades; tests: test_grades; exams: exam_grades; stu_count: out Integer) is
 temp: student;
 stu_array_index: Natural:=1;
 counter: Integer;
 tempNum: Integer;
 begin
+stu_count := 0;
 while not end_of_file loop
 counter:=1;
 get_line(temp.name, temp.letters_in_name);
@@ -208,21 +214,53 @@ temp.ppoints := compute_program_points(temp);
 temp.qpoints := compute_quiz_points(temp);
 temp.tpoints := compute_test_points(temp);
 temp.epoints := compute_exam_points(temp);
+temp.overall_average := compute_overall_average(temp);
 temp.letter_grade := computer_letter_grade(temp);
 stu_array(stu_array_index) := temp;
 stu_array_index := stu_array_index + 1;
+stu_count := stu_count + 1;
 end loop;
 end make_students;
+
+procedure print(s: student_array; stu_count: Integer) is
+begin
+for i in 1..stu_count loop
+    put_line("Name: " & s(i).name(1..s(i).letters_in_name));
+    put_line("Overall Average: " & s(i).overall_average'img);
+    put_line("Letter Grade: " & s(i).letter_grade);
+    put_line("Category          Weight  Average  Points   Grades ...");
+    put("Programs           " & s(i).pgrades.percent_total'img &"      " & s(i).paverage'img &"     " & s(i).ppoints'img & "    ");
+    for j in 1..s(i).pgrades.num_grades loop
+        put(s(i).pgrades.grades_array(j)'img);
+    end loop;
+    new_line;
+    put("Quizzes            " & s(i).qgrades.percent_total'img &"      " & s(i).qaverage'img &"     " & s(i).qpoints'img & "    ");
+    for j in 1..s(i).qgrades.num_grades loop
+        put(s(i).qgrades.grades_array(j)'img);
+    end loop;
+    new_line;
+    put("Tests              " & s(i).tgrades.percent_total'img &"      " & s(i).taverage'img &"     " & s(i).tpoints'img & "    ");
+    for j in 1..s(i).tgrades.num_grades loop
+        put(s(i).tgrades.grades_array(j)'img);
+    end loop;
+    new_line;
+    put("Final Exam         " & s(i).egrades.percent_total'img &"      " & s(i).eaverage'img &"     " & s(i).epoints'img & "    ");
+    put(s(i).egrades.exam_grade'img);
+    new_line;
+    new_line;
+end loop;
+end print;
 
 programs: program_grades;
 quizzes: quiz_grades;
 tests: test_grades;
 exams: exam_grades;
 stu_array: student_array;
+stu_count: Integer;
 begin
 make_records(programs, quizzes, tests, exams);
-make_students(stu_array, programs, quizzes, tests, exams);
-put(stu_array(3).letter_grade);
+make_students(stu_array, programs, quizzes, tests, exams, stu_count);
+print(stu_array, stu_count);
 end missing_scores;
 
 
